bdca6f55b87cd24675fe2b3437481a435045b308
Violeta Hernández <vi.hdz.p@gmail.com>
2024-09-27 18:14:05 -0600

    fix list weirdness

--- Rubik/FromPieces.lean
+++ Rubik/FromPieces.lean
@@ -1,5 +1,5 @@
 import Batteries.Data.Vector.Basic
-import Rubik.PRubik
+import Rubik.Move
 
 /-!
 We implement convenient functions `Stickers.toPRubik` and `Stickers.toRubik` to construct a Rubik's
@@ -8,8 +8,24 @@ cube from only the sticker arrangements, automatically inferring all of the rele
 This is where we also implement the `Repr` instance on `PRubik` and `Rubik`.
 -/
 
-/-- The list of stickers in a Rubik's cube. These should be given in the same order as the infoview,
-omitting the centers. That is:
+
+-- This is required in order to work with these large lists. See https://leanprover.zulipchat.com/#narrow/stream/348111-batteries/topic/Large.20vector.20hangs/near/473234634
+--
+-- This code is due to Mario in that same thread.
+open Lean
+macro_rules
+  | `([ $elems,* ]) => do
+    let rec expandListLit (i : Nat) (skip : Bool) (result : TSyntax `term) : MacroM Syntax := do
+      match i, skip with
+      | 0,   _     => pure result
+      | i+1, true  => expandListLit i false result
+      | i+1, false => expandListLit i true  (← ``(List.cons $(⟨elems.elemsAndSeps.get! i⟩) $result))
+    let size := elems.elemsAndSeps.size
+    expandListLit size (size % 2 == 0) (← ``(List.nil))
+
+open Orientation
+
+/-- The list of stickers in a Rubik's cube. These should be given in the following order:
 
 ```
          00 01 02
@@ -29,7 +45,6 @@ omitting the centers. That is:
 def Stickers : Type := Batteries.Vector Orientation 48
 
 namespace Stickers
-open Orientation
 
 instance : GetElem Stickers ℕ Orientation fun _ i => i < 48 :=
   inferInstanceAs (GetElem (Batteries.Vector Orientation 48) _ _ _)
@@ -175,7 +190,57 @@ end Stickers
 
 namespace PRubik
 
+/-- A list with all non-equivalent edges. -/
+private def Edges : List EdgePiece :=
+  [EdgePiece.mk' U B, EdgePiece.mk' U L, EdgePiece.mk' U R, EdgePiece.mk' U F,
+    EdgePiece.mk' L B, EdgePiece.mk' L F, EdgePiece.mk' F R, EdgePiece.mk' R B,
+    EdgePiece.mk' D B, EdgePiece.mk' D L, EdgePiece.mk' D R, EdgePiece.mk' D F]
+
+/-- A list with all non-equivalent corners. -/
+private def Corners : List CornerPiece :=
+  [CornerPiece.mk' U B L, CornerPiece.mk' U R B, CornerPiece.mk' U L F, CornerPiece.mk' U F R,
+    CornerPiece.mk' D L B, CornerPiece.mk' D B R, CornerPiece.mk' D F L, CornerPiece.mk' D R F]
+
+example : Batteries.Vector ℕ 31 :=
+  #v[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
 def toStickers (cube : PRubik) : Stickers :=
+  let e := Edges.map cube.edgePieceEquiv
+  let c := Corners.map cube.cornerPieceEquiv
+  have : e.length = 12 := rfl
+  have : c.length = 8 := rfl
+  -- rfl, and by extension vector notation, doesn't seem to work with this.
+  ⟨#[
+    c[0].fst, e[0].fst, c[1].fst,
+    e[1].fst,           e[2].fst,
+    c[2].fst, e[3].fst, c[3].fst,
+    c[0].thd, e[1].snd, c[2].snd,
+    e[4].fst,           e[5].fst,
+    c[4].snd, e[9].snd, c[6].thd,
+    c[2].thd, e[3].snd, c[3].snd,
+    e[5].snd,           e[6].fst,
+    c[6].snd, e[11].snd,c[7].thd,
+    c[3].thd, e[2].snd, c[1].snd,
+    e[6].snd,           e[7].fst,
+    c[7].snd, e[10].snd,c[5].thd,
+    c[6].fst, e[11].fst,c[7].fst,
+    e[9].fst,           e[10].fst,
+    c[4].fst, e[8].fst, c[5].fst,
+    c[4].thd, e[8].snd, c[5].snd,
+    e[4].snd,           e[7].snd,
+    c[0].snd, e[0].snd, c[1].thd], by simp⟩
+
+/-theorem edgeOrientations_toStickers (cube : PRubik) :
+    (toStickers cube).edgeOrientations = fun e ↦ let x := cube.edgePieceEquiv e; (x.1, x.2) := by
+  apply funext
+  intro e
+  match e with
+  | .mk (true, Axis.y)  (false, Axis.z) _ =>
+    rw [Stickers.edgeOrientations, toStickers]
+    change (List.map  cube.edgePieceEquiv PRubik.Edges)[0].fst = _ ∧ _
+    simp-/
+
+theorem toStickers_isAdjacent (cube : PRubik) : cube.toStickers.IsAdjacent :=
   sorry
 
 theorem toStickers_isProper (cube : PRubik) : cube.toStickers.IsProper :=
@@ -197,4 +262,6 @@ theorem toStickers_isProper (cube : Rubik) : (toStickers cube).IsProper :=
 instance : Repr Rubik :=
   ⟨fun c ↦ reprPrec c.1⟩
 
-end PRubik
+end Rubik
+
+
